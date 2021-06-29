a08381fe019e3776e9e4f8c924ce92a1025f6cfc
Shrihan-P <>
2021-06-29 00:32:58 -0400

    added test chart in canvas and change glassmorphing

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
 
 function started() {
+    canvas(60,20);
+}
+
+function canvas(x1, x2) {
     
+    var elemento = document.getElementById("first_chart");
+    lienzo= elemento.getContext("2d");
+    lienzo.fillRect(50,70,10,x1);
+    
+    lienzo.beginPath();
+    lienzo.moveTo(45,20);
+    lienzo.lineTo(45,135);
+    lienzo.closePath()
+    lienzo.stroke();
+    lienzo.beginPath();
+    lienzo.moveTo(45,135);
+    lienzo.lineTo(250,135);
+    lienzo.closePath()
+    lienzo.stroke();
 }
 
 
